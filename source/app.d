3b045400ba926d140a680fae391f1f9c6c23d207
Filipp Chertiev <f@fzfx.ru>
2020-03-19 19:36:55 +0300

    added some global variables

--- source/app.d
+++ source/app.d
@@ -2,6 +2,7 @@ import std.stdio;
 import std.file;
 import std.typecons;
 import std.process;
+import core.time;
 import vibe.data.json;
 import vibe.core.core;
 import vibe.core.log;
@@ -13,10 +14,32 @@ const string g_yamlConfigFileName = `config.yml`;
 Duration g_execGap;
 /// Global bot token
 string g_botToken = `1017280662:AAHu5nNmBOilY2QuRDJws0kAtqMEDCDQqL8`;
-/// Global array of values
-string[string] g_values;
+/// Global array of bots
+string[string] g_bots;
 
-void main()
+int main()
 {
-	
+	if(!g_yamlConfigFileName.exists) {
+		logError("Config file name doesn't point at any file system entry! Exitting...");
+		return -1;
+	}
+	if(!g_yamlConfigFileName.isFile) {
+		logError("Config file name doesn't point at regular file! Exitting...");
+		return -2;
+	}
+
+	Node yamlConfig = Loader.fromFile(g_yamlConfigFileName).load();
+
+	if("execGap" in yamlConfig) {
+		g_execGap = yamlConfig["execGap"].as!ushort.seconds;
+	} else {
+		g_execGap = 10.seconds;
+	}
+	if("botToken" in yamlConfig) {
+		g_botToken = yamlConfig["botToken"].as!string;
+	} else {
+		g_botToken = `unk`;
+	}
+
+	return 0;
 }
